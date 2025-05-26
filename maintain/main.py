865450d1396a73f3b8a69fca786147e975c9cb23
hbenites <harold.benites@interseguro.com.pe>
2025-05-26 00:01:37 -0500

    cloud function

--- maintain/main.py
+++ maintain/main.py
@@ -1,6 +1,11 @@
 from data_maintain import BigQueryDataManager
 
 def main(request):
-    manager = BigQueryDataManager()
-    result = manager.backup()
-    return result
+    manager = BigQueryDataManager(
+            project_id="iter-data-storage-pv-uat",
+            dataset_id="temp",
+            bucket_input="demo-input-hb",
+            bucket_backup="demo-backup-hb"
+        )
+    result = manager.backup_all()
+return result
